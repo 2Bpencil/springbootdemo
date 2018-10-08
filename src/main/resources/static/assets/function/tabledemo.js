f54ad2ca5a0fcf5ddd09a020ed4546ecfcd3e66d
tyf <63250124@qq.com>
2018-10-08 20:02:13 +0800

    更换前端模板

--- src/main/resources/static/assets/function/tabledemo.js
+++ src/main/resources/static/assets/function/tabledemo.js
@@ -2,37 +2,31 @@
 
 $(function (){
 
-
-
     pageInit();
 
 });
 function pageInit(){
+    $.jgrid.defaults.width = 1570;
+    $.jgrid.defaults.responsive = true;
+    $.jgrid.defaults.styleUI = 'Bootstrap';
     //创建jqGrid组件
-    jQuery("#dataTable").jqGrid(
-        {
-            url : contextPath+'table/getTableJson',//组件创建完成之后请求数据的url
-            datatype : "json",//请求数据返回的类型。可选json,xml,txt
-            colNames : [ 'Inv No', 'Date', 'Client', 'Amount', 'Tax','Total', 'Notes' ],//jqGrid的列显示名字
-            colModel : [ //jqGrid每一列的配置信息。包括名字，索引，宽度,对齐方式.....
-                {name : 'id',index : 'id',width : 55},
-                {name : 'invdate',index : 'invdate',width : 90},
-                {name : 'name',index : 'name asc, invdate',width : 100},
-                {name : 'amount',index : 'amount',width : 80,align : "right"},
-                {name : 'tax',index : 'tax',width : 80,align : "right"},
-                {name : 'total',index : 'total',width : 80,align : "right"},
-                {name : 'note',index : 'note',width : 150,sortable : false}
-            ],
-            rowNum : 10,//一页显示多少条
-            rowList : [ 10, 20, 30 ],//可供用户选择一页显示多少条
-            pager : '#dataPage',//表格页脚的占位符(一般是div)的id
-            sortname : 'id',//初始化的时候排序的字段
-            sortorder : "desc",//排序方式,可选desc,asc
-            mtype : "get",//向后台请求数据的ajax的类型。可选post,get
-            viewrecords : true,
-            caption : "JSON Example"//表格的标题名字
-        });
-    /*创建jqGrid的操作按钮容器*/
-    /*可以控制界面上增删改查的按钮是否显示*/
-    jQuery("#dataTable").jqGrid('navGrid', '#pager2', {edit : false,add : false,del : false});
+    $("#dataTable").jqGrid({
+        url: 'http://trirand.com/blog/phpjqgrid/examples/jsonp/getjsonp.php?callback=?&qwery=longorders',
+        mtype: "GET",
+        styleUI : 'Bootstrap',
+        multiselect: true,//复选框
+        datatype: "jsonp",
+        colModel: [
+            { label: 'OrderID', name: 'OrderID', key: true, width: 75 },
+            { label: 'Customer ID', name: 'CustomerID', width: 150 },
+            { label: 'Order Date', name: 'OrderDate', width: 150 },
+            { label: 'Freight', name: 'Freight', width: 150 },
+            { label:'Ship Name', name: 'ShipName', width: 150 }
+        ],
+        viewrecords: true,
+        height: 250,
+        rowNum: 10,
+        rowList : [10,20,30],
+        pager: "#dataPage"
+    });
 }
